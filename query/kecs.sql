9287e4593bde69bbe15b4bfdcee4b1858ddb128a
Connor Newton <connor@ifthenelse.io>
2016-08-21 20:43:28 +0100

    Fixed a bunch of issues I mistakenly introduced into the KECS query procedure.

--- query/kecs.sql
+++ query/kecs.sql
@@ -4,7 +4,7 @@ DROP PROCEDURE IF EXISTS kecs;
 
 CREATE PROCEDURE kecs
 (
-	IN username CHAR(20) CHARACTER SET ascii,
+	IN username_ CHAR(20) CHARACTER SET ascii,
 	IN t1 INT UNSIGNED,
 	IN t2 INT UNSIGNED
 )
@@ -16,7 +16,7 @@ BEGIN
 	################
 
 	-- rename our input variable to avoid namespace collisions
-	SET @author = username;
+	SET @author = username_;
 	SET @t1 = t1;
 	SET @t2 = t2;
 
@@ -25,11 +25,11 @@ BEGIN
 	START TRANSACTION;
 
 	-- set our session's 'temporary' table name
-	SET @descendant_comments = CONCAT('descendant_comments_s',connection_id());
+	SET @comment_recu = CONCAT('comment_recu_s',connection_id());
+	SET @comment_desc = CONCAT('comment_desc_s',connection_id());
 
 	-- get the user's author_id
-	SELECT id INTO @author_id FROM author WHERE username = @author;
-
+	SELECT author_id INTO @author_id FROM author WHERE username = @author;
 
 	#################
 	## POST SEARCH ##
@@ -41,7 +41,7 @@ BEGIN
 
 	-- create the table
 	SET @sql = CONCAT(
-	   'CREATE TABLE IF NOT EXISTS ',@descendant_comments,' (
+	   'CREATE TABLE IF NOT EXISTS ',@comment_recu,' (
 			comment_id BIGINT UNSIGNED NOT NULL,
 			depth INT UNSIGNED NOT NULL,
 			PRIMARY KEY (comment_id),'
@@ -56,10 +56,10 @@ BEGIN
 	-- find only author comments that aren't in their own submissions
 	SET @depth = 0;
 	SET @sql = CONCAT(
-	   'INSERT INTO ',@descendant_comments,'
+	   'INSERT INTO ',@comment_recu,'
 		SELECT co.comment_id,@depth
-		FROM comments_meta co
-		INNER JOIN submissions_meta su
+		FROM comment co
+		INNER JOIN submission su
 		ON co.link_id = su.link_id
 		WHERE co.author_id = @author_id
 		AND su.author_id != @author_id'
@@ -74,17 +74,17 @@ BEGIN
 
 	-- fetch descendants of comments found in previous iteration
 	SET @sql = CONCAT(
-	   'INSERT IGNORE INTO ',@descendant_comments,'
+	   'INSERT IGNORE INTO ',@comment_recu,'
 		SELECT co.comment_id,(? + 1)
-		FROM comments_meta co
-		INNER JOIN ',@descendant_comments,' de
+		FROM comment co
+		INNER JOIN ',@comment_recu,' de
 		ON  co.parent_id = de.comment_id
 		WHERE de.depth = ?'
 	);
 	PREPARE insert_next FROM @sql;
 
 	-- continue the recursion until there are no new descendants to find children of
-	SET @sql = CONCAT('SELECT EXISTS (SELECT comment_id FROM ',@descendant_comments,' WHERE depth = ?) INTO @new_descendants_exist');
+	SET @sql = CONCAT('SELECT EXISTS (SELECT comment_id FROM ',@comment_recu,' WHERE depth = ?) INTO @new_descendants_exist');
 	PREPARE new_descendants_query FROM @sql;
 
 	-- perform the recursive search
@@ -107,10 +107,10 @@ BEGIN
 
 	-- create the table
 	SET @sql = CONCAT(
-	   'CREATE TABLE ',@result_posts,' (
+	   'CREATE TABLE ',@comment_desc,' (
 		   ' -- post_id BIGINT UNSIGNED NOT NULL,
 		   'is_self TINYINT UNSIGNED NOT NULL,
-			score MEDIUMINT NOT NULL,
+			score INT NOT NULL,
 			KEY (is_self) USING BTREE
 		) ENGINE=MEMORY
 		SELECT a.is_self,a.score FROM
@@ -120,7 +120,7 @@ BEGIN
 		   'SELECT
 		   		1 AS is_self,
 		   		su.score AS score
-		   	FROM submissions_meta su
+		   	FROM submission su
 			WHERE su.author_id = @author_id
 			AND su.created_utc >= @t1
 			AND su.created_utc < @t2
@@ -131,8 +131,8 @@ BEGIN
 		   'SELECT
 				co.author_id=@author_id AS is_self,
 				co.score AS score
-			FROM comments_meta co
-			INNER JOIN submissions_meta su
+			FROM comment co
+			INNER JOIN submission su
 			ON co.link_id = su.link_id
 			WHERE su.author_id = @author_id
 			AND co.created_utc >= @t1
@@ -143,8 +143,8 @@ BEGIN
 		   'SELECT
 				co.author_id=@author_id AS is_self,
 				co.score AS score
-			FROM comments_meta co
-			INNER JOIN ',@descendant_comments,' de
+			FROM comment co
+			INNER JOIN ',@comment_recu,' de
 			ON co.comment_id = de.comment_id
 			WHERE co.created_utc >= @t1
 			AND co.created_utc < @t2
@@ -155,27 +155,31 @@ BEGIN
 	EXECUTE create_tbl;
 	DEALLOCATE PREPARE create_tbl;
 
-	-- select final scores
 	SET @sql = CONCAT('
-		SELECT
-			@author,
-			(SELECT coalesce(sum(score),0) FROM ',@result_posts,'),
-			(SELECT coalesce(sum(score),0) FROM ',@result_posts,' WHERE is_self = 1),
-			(SELECT count(1) FROM ',@result_posts,'),
-			(SELECT count(1) FROM ',@result_posts,' WHERE is_self = 1)'
+		SELECT * FROM ',@comment_desc
 	);
 	PREPARE select_results FROM @sql;
 	EXECUTE select_results;
 	DEALLOCATE PREPARE select_results;
 
-
-	##################
-	## DEINITIALIZE ##
-	##################
-
-	SET @sql = CONCAT('DROP TABLE IF EXISTS ',@descendant_comments);
+	-- don't hold table for any longer than is necessary
+	SET @sql = CONCAT('DROP TABLE IF EXISTS ',@comment_recu);
 	PREPARE drop_tbl FROM @sql; EXECUTE drop_tbl; DEALLOCATE PREPARE drop_tbl;
-	SET @sql = CONCAT('DROP TABLE IF EXISTS ',@result_posts);
+
+	-- select final scores
+	SET @sql = CONCAT('
+		SELECT
+			@author AS username,
+			(SELECT coalesce(sum(score),0) FROM ',@comment_desc,') AS score,
+			(SELECT coalesce(sum(score),0) FROM ',@comment_desc,' WHERE is_self = 1) AS score_self,
+			(SELECT count(1) FROM ',@comment_desc,') AS count,
+			(SELECT count(1) FROM ',@comment_desc,' WHERE is_self = 1) AS count_self'
+	);
+	PREPARE select_results FROM @sql;
+	EXECUTE select_results;
+	DEALLOCATE PREPARE select_results;
+
+	SET @sql = CONCAT('DROP TABLE IF EXISTS ',@comment_desc);
 	PREPARE drop_tbl FROM @sql; EXECUTE drop_tbl; DEALLOCATE PREPARE drop_tbl;
 
 	COMMIT;
