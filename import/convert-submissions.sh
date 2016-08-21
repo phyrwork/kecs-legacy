73c7acf3b98e106444b5b3e8959d00cc1881d76d
Connor Newton <connor@ifthenelse.io>
2016-08-21 18:32:58 +0100

    Added file overwrite guard to conversion scripts.

--- import/convert-submissions.sh
+++ import/convert-submissions.sh
@@ -5,6 +5,12 @@ do
     in=${file} ;
     out=${file//json/csv} ;
 
+    if [ "$out" = "$in" ]
+	then
+		echo "Output file will overwrite input file. Possible cause: input file not '.json'. Aborting!"
+		exit 1;
+	fi
+
     cat ${in} | json2csv -k id,created_utc,author,score -p > ${out} ;
 
  done
--- import/unzip-split.sh
+++ import/unzip-split.sh
@@ -4,6 +4,12 @@ do
 	echo "Decompressing "${file}"..." ;
 	outfile=${file//bz2/json} ;
 
+	if [ "$outfile" = "$file" ]
+	then
+		echo "Output file will overwrite input file. Possible cause: input file not '.bz2'. Aborting!"
+		exit 1;
+	fi
+
 	case $(uname) in
 		"Darwin") cat ${file} | pbzip2 -c -d | split -l 165000 -a 2 - ${outfile}. ;; # split -b splits before the end of the line. Approx. 165K lines per 256M
 		"Linux")  cat ${file} | pbzip2 -c -d | split -C 256M   -a 2 - ${outfile}. ;;
