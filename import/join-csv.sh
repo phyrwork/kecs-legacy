496cb2439c7563a51a0c176ecf8f60c128ada52c
Connor Newton <connor@ifthenelse.io>
2016-08-28 08:07:40 +0100

    Fixed a small assignment error in join-csv.sh. Added some progress output.

--- import/join-csv.sh
+++ import/join-csv.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 source ../env.sh;
 
-output = $1;
+output=$1;
 shift;
+
+echo "Joining CSVs to $output...";
 
 head -n 1 $1 >> $output;
 
 while (( "$#" )); do
 
+	echo "...appending $1";
 	tail -n +2 $1 >> $output;
 	shift
 
 done
+
+echo "Done!";
