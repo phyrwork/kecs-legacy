5eaa66de656130411def0021cdcc571ca54628be
Connor Newton <connor@ifthenelse.io>
2016-08-30 21:52:03 +0100

    Upgraded kecs and pkecs scripts to take named arguments.

--- bin/kecs.sh
+++ bin/kecs.sh
@@ -1,9 +1,71 @@
-host=$1;
-database=$2;
-author=$3;
-time_start=$4;
-time_end=$5;
+#!/bin/bash
+
+# defaults
+host="";
+database="";
+time_start=0;
+time_end=2147483647;
+header=false;
+author="";
+
+# parse arguments
+while [[ $# -gt 0 ]]
+do
+	key=$1;
+	case $key in
+		-h|--host)
+			shift;
+			host=$1; shift ;;
+
+		-d|--database)
+			shift;
+			database=$1; shift ;;
+
+		-a|--after)
+			shift;
+			time_start=$1; shift ;;
+
+		-b|--before)
+			shift;
+			time_end=$1; shift ;;
+
+		-k|--header)
+			shift;
+			header=true ;;
+
+		*)
+			author=$1; break ;;
+	esac
+done
+
+# validate options
+if [ "$host" == "" ]
+then
+	echo "Error: No host specified. Exiting!";
+	exit -1;
+fi
+
+if [ "$database" == "" ]
+then
+	echo "Error: No host specified. Exiting!";
+	exit -1;
+fi
+
+if [ "$author" == "" ]
+then
+	echo "Error: No author specified. Exiting!";
+	exit -1;
+fi
+
+# prepare
+opts="-h $host";
+if [ $header = false ]
+then
+	opts+=" -N";
+fi
+opts+=" $database";
 
 sql="CALL kecs('$author',$time_start,$time_end)";
 
-mysql -h$host $database <<< $sql | tail -n +2;
+# execute
+mysql $opts <<< $sql;
--- bin/pkecs.sh
+++ bin/pkecs.sh
@@ -1,8 +1,63 @@
-host=$1;
-database=$2;
-author_file=$3;
-time_start=$4;
-time_end=$5;
+#!/bin/bash
 
-echo "username\tscore\tcount\tscore_self\tcount_self";
-parallel --linebuffer --progress --xapply -j16 ./kecs.sh ::: $1 ::: $2 :::: $3 ::: $4 ::: $5
+# defaults
+host="";
+database="";
+time_start=0;
+time_end=2147483647;
+
+# parse arguments
+while [[ $# -gt 0 ]]
+do
+	key=$1;
+	case $key in
+		-h|--host)
+			shift;
+			host=$1; shift ;;
+
+		-d|--database)
+			shift;
+			database=$1; shift ;;
+
+		-a|--after)
+			shift;
+			time_start=$1; shift ;;
+
+		-b|--before)
+			shift;
+			time_end=$1; shift ;;
+
+		-k|--header)
+			shift;
+			header=true ;;
+
+		*)
+			author_file=$1; break ;;
+	esac
+done
+
+# validate options
+if [ "$host" == "" ]
+then
+	echo "Error: No host specified. Exiting!";
+	exit -1;
+fi
+
+if [ "$database" == "" ]
+then
+	echo "Error: No host specified. Exiting!";
+	exit -1;
+fi
+
+if [ "$author_file" == "" ]
+then
+	echo "Error: No authors file specified. Exiting!";
+	exit -1;
+fi
+
+# execute
+if [ $header = true ]
+then
+	echo "username\tscore\tcount\tscore_self\tcount_self";
+fi
+parallel -N4 --linebuffer --progress --xapply -j16 kecs -h {1} -d {2} -a {3} -b {4} {5} ::: "$host" ::: "$database" ::: "$time_start" ::: "$time_end" :::: "$author_file" ;
