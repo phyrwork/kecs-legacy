b38c5873605d3dd59efe1bb06415d8e40c01620f
Connor Newton <connor@ifthenelse.io>
2016-09-01 13:10:24 +0100

    Added MATLAB wrapper for kecs.sh

---
+++ matlab/+kecs/serial.m
@@ -0,0 +1,32 @@
+function T = serial( host,database,author,time_start,time_end )
+%KECS Call KECS procedure
+
+    % defaults
+    if nargin < 4
+        time_start = 0;
+    end
+    
+    if nargin < 5
+        time_end = 2147483647;
+    end
+    
+    % build command
+    cmd = sprintf('%s ',...
+        'kecs',...
+        '-h',host,...
+        '-d',database,...
+        '-a',num2str(time_start),...
+        '-b',num2str(time_end),...
+        '-k',...
+        author...
+    );
+    
+    % execute
+    [~,output] = unix(cmd);
+    
+    % parse results
+    [columns,pos] = textscan(output,'%s\t%s\t%s\t%s\t%s',1);
+    data = textscan(output(pos+1:end),'%s\t%u\t%u\t%u\t%u');
+    T = table(data{:},'VariableNames',[columns{:}]);
+end
+
